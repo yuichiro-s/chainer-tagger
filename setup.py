4225a57cbdc6768ea0106ee5d4153d7b5e90a392
yuichiro-s <sawai.yuichiro.sn0@is.naist.jp>
2015-10-05 17:39:20 +0900

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='chainer-tagger',
+    version='0.0.1',
+    description='Chainer-based POS tagger',
+    author='Yuichiro Sawai',
+    author_email='sawai.yuichiro.sn0@is.naist.jp',
+    packages=['ctagger'],
+)
