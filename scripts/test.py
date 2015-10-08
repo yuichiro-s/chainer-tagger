7901137139fa5d8b2daab7b08b411692add6bf6e
yuichiro-s <sawai.yuichiro.sn0@is.naist.jp>
2015-10-08 17:55:52 +0900

    Fix typo

--- scripts/test.py
+++ scripts/test.py
@@ -9,6 +9,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Train NN tagger.')
 
     parser.add_argument('data', help='path to test data')
-    parser.add_argument('model', help='destination of model')
+    parser.add_argument('model', help='path to model')
 
     test.test(parser.parse_args())
+
